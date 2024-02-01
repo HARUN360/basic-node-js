01f53603880b41c8a3f7d5a0e3fbcfcf55d3b764
HARUN360 <mdharun573192@gmail.com>
2024-02-01 17:40:38 +0600

    object structure add

--- object.js
+++ object.js
@@ -1,6 +1,34 @@
-const num = {
-    node:'a',
-    node1:'b'
+// 1.No: object declaration --------------------->
+
+// 1.1 Object Literal Syntax
+// const MyInfo = {
+//   name:"MD Harun",
+//   department: 'cse',   /* name gulake bola hoi name = property/key */
+//   aim:"devolopment",   /* Md Harun gulake bola hoi name = value */
+//   age:18,
+//   marid:false,
+// }
+// console.log(MyInfo);
+
+
+
+// 1.2 Using the Object Constructor:
+// const MyInfo = new Object();
+// MyInfo.name = 'MD Harun';
+// MyInfo.department = 'cse';
+// MyInfo.aim = 'cse';
+// MyInfo.age = 18;
+// MyInfo.marid = false;
+// console.log(MyInfo);
+
+
+// 1.3 Using a Constructor Function:
+function MyInfo(name, department, aim, age, marid ) {
+ this.name = name;
+ this.department = department;
+this.aim =aim;
+this.age = age;
+this.marid = marid;
 }
-console.log(num);
-console.log("md harun or roshid");
+const ShowInfo = new MyInfo('Md Harun or roshid', 'cse', 'as webdevoloper', 18, false)
+console.log(ShowInfo);
