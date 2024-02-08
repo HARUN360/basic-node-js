bffebfa74bcadeda653c53c3fe4f1c04c3844708
HARUN360 <mdharun573192@gmail.com>
2024-02-08 13:48:32 +0600

    assing part

--- problems1.js
+++ problems1.js
@@ -474,7 +474,7 @@
 // -----------------------------------------------------end-----------------------------------------------------------------
 
 
-// 8.4(akti array ar bitore akadik object theke price ar avarage nirnoy)?-------------------------------------------------------------------->
+// 15 (akti array ar bitore akadik object theke price ar avarage nirnoy)?-------------------------------------------------------------------->
 // function findAveragePhonePrice(nums) {
 //     let priceArray = [];
 //     for (const num of nums) {
@@ -528,3 +528,82 @@
 // const result = peresentSalary(employees);
 // console.log( 'compmani total employcost = ', result);
 // -------------------------------------------------------------end-------------------------------------------------
+
+
+// Problem-01 : Help The Zoo Manager ----------------------------------------------------------------------
+// function calculateMoney(ticketSale) {
+//     if (typeof ticketSale !== 'number' || ticketSale < 0) {
+//         return 'error: “Invalid Number” ticket should be a valid positive number';
+//     }
+//     const saleTicketPrice = ticketSale * 120;
+//     const daruanSelary = 500;
+//     const stuffLunch = 8 * 50;
+//     const totalCost = daruanSelary + stuffLunch;
+//     const totalEarn = saleTicketPrice - totalCost;
+//     return totalEarn;
+// }
+// const profit = calculateMoney(93);
+// console.log(profit);
+// -------------------------------------------------end-------------------------------------------------------
+
+// Problem-02: Good Name , Bad Name -------------------------------------------------------------------------->
+// function checkName(name) {
+//     if (typeof name !== 'string') {
+//         return 'invalid:please provide a string';
+//     }
+//     const nameLowerCase = name.toLowerCase();
+//     const lastLetter = nameLowerCase[nameLowerCase.length - 1];
+//     const goodLastLetter = 'ayieouw';
+//     if (goodLastLetter.includes(lastLetter)) {
+//         return 'Good Name';
+//     }else {
+//         return 'Bad Name';
+//     }
+// }
+// const result = checkName('RAFEE');
+// console.log(result);
+// --------------------------------------------------end------------------------------------------------------
+
+// Problem 03 : Virus in my Array-----------------------------------------------------------------------------
+// function deleteInvalids(array) {
+//     if (!Array.isArray(array)) {
+//         return ' “Invalid Array” please  provide a array';
+//     }
+//     const newArray = [];
+//     for (const arrays of array) {   
+//         if (typeof arrays === 'number' && !isNaN(arrays)) {
+//             newArray.push(arrays);
+//         }
+//     }
+//     return newArray;
+// }
+// const arrayValue =[1 , null, undefined, 18, -19, NaN, "12", [1, 2], { ob: "lala" }];
+// const result = deleteInvalids(arrayValue);
+// console.log(result);
+// ------------------------------------------------------end---------------------------------------------------
+
+
+
+// Problem 04 : Make A Great Password:-------------------------------------------------------------------------
+// function password(obj) {
+//     if (typeof obj !== 'object') {
+//         return 'invalid:please provide an object'
+//     }
+//     const keys = Object.keys(obj);
+//     if (!keys.includes('name') || (!keys.includes('birthYear')) || (!keys.includes('siteName'))) {
+//         return 'invalid:participants should be a name,birthYear and siteName'
+//     }
+//     const birthYearString = obj.birthYear.toString();
+//     const birthYearLength = birthYearString.length;
+//     if(birthYearLength !== 4){
+//         return 'invalid:please provide birthYear 4 digit';
+//     }
+//     obj.siteName = obj.siteName[0].toUpperCase() + obj.siteName.slice(1);
+//     const result = obj.siteName + '#' + obj.name + '@' + obj.birthYear;
+//     return result;
+// }
+// const ob = { name: 'kolimuddin' , birthYear: 1999 , siteName: 'google' }
+// const result = password(ob);
+// console.log(result);
+// -----------------------------------------end--------------------------------------------------------------
+
